74fe556fe1db3401d60f93bb351fd2013ec8e561
mohammed shahnawaz <shah4corp@gmail.com>
2024-05-24 21:25:34 +0530

    24052024 second commit

---
+++ Selenium/src/test/java/com/Practice/Selenium052024/NewSeleniumProgram.java
@@ -0,0 +1,13 @@
+package com.Practice.Selenium052024;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewSeleniumProgram {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("");
+	}
+
+}
