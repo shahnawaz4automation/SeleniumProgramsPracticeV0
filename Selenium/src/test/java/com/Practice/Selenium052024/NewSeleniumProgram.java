d68f53fe39e2b323ba7c65fcb03954a384f2b885
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-05-25 13:17:03 +0530

    25052024 fifth commit

--- Selenium/src/test/java/com/Practice/Selenium052024/NewSeleniumProgram.java
+++ Selenium/src/test/java/com/Practice/Selenium052024/NewSeleniumProgram.java
@@ -1,31 +1,27 @@
 package com.Practice.Selenium052024;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NewSeleniumProgram {
 
 	public static void main(String[] args) throws InterruptedException, MalformedURLException {
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
 
-		//driver.get("https://omayo.blogspot.com/");
-		URL url = new URL("https://omayo.blogspot.com/");
-		driver.navigate().to(url);
+		driver.get("https://omayo.blogspot.com/");
 		WebElement button1 = driver.findElement(By.xpath("//button[contains(text(),'Button2')]"));
-				
+		
+		button1.isDisplayed();
+		
 		driver.navigate().back();
-		Thread.sleep(10000);
+		Thread.sleep(1000);
 		driver.navigate().forward();
-		Thread.sleep(10000);
+		Thread.sleep(1000);
 		driver.navigate().refresh();
 
 	}
