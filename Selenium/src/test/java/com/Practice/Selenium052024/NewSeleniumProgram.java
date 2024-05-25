5b40f6032f57b0b014932d80516745927b5a4c37
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-05-25 07:52:44 +0530

    25052025 third commit

--- Selenium/src/test/java/com/Practice/Selenium052024/NewSeleniumProgram.java
+++ Selenium/src/test/java/com/Practice/Selenium052024/NewSeleniumProgram.java
@@ -1,20 +1,22 @@
 package com.Practice.Selenium052024;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewSeleniumProgram {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https:\\www.google.com");
-		
-		Thread.sleep(1000);
-		
-		driver.quit();
-		driver.close();
-		// Testing the pull request
-		//driver.close();
+		driver.get("https://www.google.com");
+
+		driver.get("https://omayo.blogspot.com/");
+		WebElement button1 = driver.findElement(By.xpath("//button[contains(text(),'Button2')]"));
+		System.out.println(button1.isDisplayed());
+		System.out.println(button1.isEnabled());
+		System.out.println(button1.isSelected());
+
 	}
 
 }
