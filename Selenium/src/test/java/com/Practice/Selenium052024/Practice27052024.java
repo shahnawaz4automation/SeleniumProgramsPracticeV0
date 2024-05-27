514831f7be5dcd03bc2dc43a248714c93cd267a3
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-05-27 21:25:23 +0530

    27052024 third commit

--- Selenium/src/test/java/com/Practice/Selenium052024/Practice27052024.java
+++ Selenium/src/test/java/com/Practice/Selenium052024/Practice27052024.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Practice27052024 {
@@ -16,7 +17,11 @@ public class Practice27052024 {
 		
 		driver.findElement(By.xpath("//*[@id=\"Blog1\"]/div[4]/div/a")).click();
 		
-		driver.quit();
+		driver.switchTo().newWindow(WindowType.WINDOW);
+		// Opens LambdaTest homepage in the newly opened window
+		driver.navigate().to("https://www.lambdatest.com/");
+		
+		driver.close();driver.quit();
 	}
 
 }
