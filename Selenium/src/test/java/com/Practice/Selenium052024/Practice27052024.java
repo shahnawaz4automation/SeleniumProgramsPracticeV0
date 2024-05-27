e0e6d0901f854de82dd565a23cdafc6de2a5d86b
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-05-27 21:34:32 +0530

    27052024 sixth commit

--- Selenium/src/test/java/com/Practice/Selenium052024/Practice27052024.java
+++ Selenium/src/test/java/com/Practice/Selenium052024/Practice27052024.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Practice27052024 {
