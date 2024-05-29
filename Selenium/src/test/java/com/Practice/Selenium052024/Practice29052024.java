d3234f65295949f768c186cd31399a0d57087931
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-05-29 22:47:46 +0530

    29052024 third commit

--- Selenium/src/test/java/com/Practice/Selenium052024/Practice29052024.java
+++ Selenium/src/test/java/com/Practice/Selenium052024/Practice29052024.java
@@ -2,17 +2,24 @@ package com.Practice.Selenium052024;
 
 import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class Practice29052024 {
-	@Test
-	public void practice(){
-		ChromeDriver driver = new ChromeDriver();
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://tutorialsninja.com/demo/");
-		driver.manage().window().fullscreen();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
-		//driver
+		String name = driver.getClass().getName();
+		System.out.println("name is: "+name);
+		
+		String simplename = driver.getClass().getSimpleName();
+		System.out.println("simple name is: "+simplename);
+		System.out.println(Practice29052024.class.getSimpleName());
+		
+		driver.quit();
 	}
 	
 
