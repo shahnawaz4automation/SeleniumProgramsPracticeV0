80e3a54e3a6235a5b7f37f7789f02ac294d812be
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-06 22:02:32 +0530

    22:02 06062024 commit

--- Selenium/src/test/java/com/Practice/Selenium062024/ExplicitWaitPractice1.java
+++ Selenium/src/test/java/com/Practice/Selenium062024/ExplicitWaitPractice1.java
@@ -1,10 +1,13 @@
 package com.Practice.Selenium062024;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ExplicitWaitPractice1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 	}
 
 }
