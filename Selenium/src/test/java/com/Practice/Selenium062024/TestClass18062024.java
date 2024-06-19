cee12c80d4c1f4be66d8b73c6235d2efa6322cf5
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-19 23:14:09 +0530

    7999819062313 commit

--- Selenium/src/test/java/com/Practice/Selenium062024/TestClass18062024.java
+++ Selenium/src/test/java/com/Practice/Selenium062024/TestClass18062024.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -20,14 +21,20 @@ public class TestClass18062024 {
 		Options option = driver.manage();
 		Timeouts timeout = option.timeouts();
 		timeout.implicitlyWait(Duration.ofSeconds(10));
-		
+
 		driver.get("https://www.google.com");
+		TestClass18062024.takeScreeshot(driver);
+		System.out.println("Screenshot is taken, refresh the directory and check");
+	
+		driver.quit();
+	}
+
+	public static void takeScreeshot(WebDriver driver) throws IOException {
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File src = ts.getScreenshotAs(OutputType.FILE);
-		File trg = new File(".\\screenshots\\homepage.png");
+		String str = RandomStringUtils.randomAlphabetic(5);
+		File trg = new File("./screenshots/"+str+"homepage.png");
 		FileUtils.copyFile(src, trg);
-		
-		System.out.println("Screenshot is taken, refresh the directory and check");
 	}
 
 }
