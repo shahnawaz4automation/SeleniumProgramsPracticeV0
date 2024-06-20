42920a2826564fb42f52f537d69b16d27b0c5e26
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-20 22:34:46 +0530

    67876223420062024 commit

--- Selenium/src/test/java/com/Practice/Selenium062024/TestClass18062024.java
+++ Selenium/src/test/java/com/Practice/Selenium062024/TestClass18062024.java
@@ -6,12 +6,15 @@ import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.WebDriver.Timeouts;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class TestClass18062024 {
 
@@ -25,15 +28,24 @@ public class TestClass18062024 {
 		driver.get("https://www.google.com");
 		TestClass18062024.takeScreeshot(driver);
 		System.out.println("Screenshot is taken, refresh the directory and check");
-	
-		driver.quit();
+
+		WebElement googleimg = driver.findElement(By.xpath("//img[@src='/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png']"));
+		TestClass18062024.elementScreeshot(googleimg);
+
 	}
 
 	public static void takeScreeshot(WebDriver driver) throws IOException {
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File src = ts.getScreenshotAs(OutputType.FILE);
 		String str = RandomStringUtils.randomAlphabetic(5);
-		File trg = new File("./screenshots/"+str+"homepage.png");
+		File trg = new File("./screenshots/" + str + "homepage.png");
+		FileUtils.copyFile(src, trg);
+	}
+
+	public static void elementScreeshot(WebElement element) throws IOException {
+		File src = element.getScreenshotAs(OutputType.FILE);
+		String str = RandomStringUtils.randomAlphabetic(5);
+		File trg = new File("./screenshots/" + str + "homepage.png");
 		FileUtils.copyFile(src, trg);
 	}
 
