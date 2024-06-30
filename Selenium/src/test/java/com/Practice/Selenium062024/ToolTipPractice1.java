382baa5bcf1b34efe498a27a4a4e5e3baf25632e
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-30 20:54:41 +0530

    78909205430062024 commit

--- Selenium/src/test/java/com/Practice/Selenium062024/ToolTipPractice1.java
+++ Selenium/src/test/java/com/Practice/Selenium062024/ToolTipPractice1.java
@@ -1,7 +1,12 @@
 package com.Practice.Selenium062024;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ToolTipPractice1 {
@@ -9,6 +14,14 @@ public class ToolTipPractice1 {
 	public void validateToolTip() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://seleniumpractise.blogspot.com/2019/09/bootstrap-tooltip-in-selenium.html");
+		
+		WebElement elementToMouseHover = driver.findElement(By.xpath("//a[text()='Hover over me']"));
+		Actions act = new Actions(driver);
+		act.moveToElement(elementToMouseHover).perform();
+		
+		//driver.quit();
 	}
 	
 }
