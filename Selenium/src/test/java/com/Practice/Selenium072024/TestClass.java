e1251daf3e0dda780bec6637b8f9baee3fe3c43c
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-03 22:31:01 +0530

    6789030724 commit

--- Selenium/src/test/java/com/Practice/Selenium072024/TestClass.java
+++ Selenium/src/test/java/com/Practice/Selenium072024/TestClass.java
@@ -6,5 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestClass {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 	}
 }
