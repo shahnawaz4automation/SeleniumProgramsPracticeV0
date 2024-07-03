326a1622fb2e3cfafb935b4f393f19869b302e1a
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-03 22:30:31 +0530

    532434030724 commit

---
+++ Selenium/src/test/java/com/Practice/Selenium072024/TestClass.java
@@ -0,0 +1,10 @@
+package com.Practice.Selenium072024;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+	}
+}
