ca7ff1d79ae3b1e0b986d1364d6d9c480d27e314
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-06 15:00:17 +0530

    BootStrapDropDown updated

--- Selenium/src/test/java/com/Practice/Selenium072024/BootStrapDropDown.java
+++ Selenium/src/test/java/com/Practice/Selenium072024/BootStrapDropDown.java
@@ -1,9 +1,11 @@
 package com.Practice.Selenium072024;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BootStrapDropDown {
@@ -13,11 +15,20 @@ public class BootStrapDropDown {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
-		// //ul[@class='multiselect-container dropdown-menu']//li//a//label//input
 		
 		driver.findElement(By.xpath("//button[@class='multiselect dropdown-toggle btn btn-default']")).click();
 		
-		driver.findElement(By.xpath("//input[@value='Angular']")).click();
+		//driver.findElement(By.xpath("//input[@value='Angular']")).click();
 		
+		List<WebElement> dropDownValues = driver.findElements(By.xpath("//ul[@class='multiselect-container dropdown-menu']//li//a//label"));
+		int sizeOfValues = dropDownValues.size();
+		System.out.println(sizeOfValues);
+		for(WebElement options: dropDownValues) {
+			String text = options.getText();
+			
+			if(text.contains("Angular")||text.contains("Java")||text.contains("Bootstrap")) {
+				options.click();
+			}
+		}
 	}
 }
