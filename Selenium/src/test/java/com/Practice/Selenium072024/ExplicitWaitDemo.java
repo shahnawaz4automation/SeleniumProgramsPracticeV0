c41ee98e8ed5cd010f3c678af9d1e9c5bff21ce9
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-14 21:18:40 +0530

    243422118140724 commit

--- Selenium/src/test/java/com/Practice/Selenium072024/ExplicitWaitDemo.java
+++ Selenium/src/test/java/com/Practice/Selenium072024/ExplicitWaitDemo.java
@@ -1,14 +1,12 @@
 package com.Practice.Selenium072024;
 
 import java.time.Duration;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExplicitWaitDemo {
@@ -22,18 +20,6 @@ public class ExplicitWaitDemo {
 		By locator = By.xpath("//img[@class='lnXdpd']");
 		waitForElementPresent(driver, locator, 10).sendKeys("selenium");
 		
-		// Waiting 30 seconds for an element to be present on the page, checking
-		   // for its presence once every 5 seconds.
-		   Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-		       .withTimeout(Duration.ofSeconds(30))
-		       .pollingEvery(Duration.ofSeconds(5))
-		       .ignoring(NoSuchElementException.class);
-
-		   WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
-		     public WebElement apply(WebDriver driver) {
-		       return driver.findElement(By.xpath("//img[@class='lnXdpd']");
-		     }
-		   });
 	}
 	
 	public static WebElement waitForElementPresent(WebDriver driver, By locator, int timeout) {
