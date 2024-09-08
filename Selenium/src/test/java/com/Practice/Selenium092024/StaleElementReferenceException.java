06cbd660998ffc4fcf7423346ab9559674c1f144
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-09-08 15:02:33 +0530

    1502204 commit

---
+++ Selenium/src/test/java/com/Practice/Selenium092024/StaleElementReferenceException.java
@@ -0,0 +1,43 @@
+package com.Practice.Selenium092024;
+
+import java.io.File;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class StaleElementReferenceException {
+	
+	private static Logger logger = LogManager.getLogger();
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://demo.guru99.com/v4/");
+		logger.info("URL launched");
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement UserId =wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("uid")));
+		UserId.sendKeys("mngr587183");
+		
+		logger.info("username entered");
+		
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File trg = new File("./screenshots/shahid.png");
+		FileUtils.copyFile(src, trg);
+		
+		driver.quit();
+
+	}
+}
