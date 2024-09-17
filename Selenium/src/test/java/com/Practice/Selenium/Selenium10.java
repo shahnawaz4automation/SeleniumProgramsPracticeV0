f3f958c0c54f284be43bc0cae456a31b2e655038
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-09-17 23:24:19 +0530

    424332 commit

--- Selenium/src/test/java/com/Practice/Selenium/Selenium10.java
+++ Selenium/src/test/java/com/Practice/Selenium/Selenium10.java
@@ -5,14 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-//import io.qameta.allure.Description;
+import io.qameta.allure.Description;
+
 public class Selenium10 {
 	// Open app.vwo.com
 	// Print the title and get the Current URL
 	// VerIfy the Current URL is app.vwo.com
 
 	@Test(groups = "QA")
-	//@Description("Verfiy the current URl, title of VWO app")
+	@Description("Verfiy the current URl, title of VWO app")
 	public void testVWOLogin() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://app.vwo.com");
