86b5d68e15dbc6f32f028036c34b67aead753231
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-09-28 15:04:36 +0530

    4324324 commit

--- Selenium/src/test/java/com/Practice/Selenium092024/CloseFunctionTest.java
+++ Selenium/src/test/java/com/Practice/Selenium092024/CloseFunctionTest.java
@@ -1,5 +1,6 @@
 package com.Practice.Selenium092024;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CloseFunctionTest {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		Dimension d = new Dimension(1466, 100);
+		driver.manage().window().setSize(d);
 		driver.get("https://www.google.com");
 		driver.switchTo().newWindow(WindowType.WINDOW);
 		driver.get("https://www.facebook.com");
