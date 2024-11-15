77c8c481c4cf9f88e57380ec3dc2466e0c3c751e
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-11-15 20:31:36 +0530

    423rfqf commit

---
+++ Selenium/src/test/java/com/Practice/Selenium112024/TestExample.java
@@ -0,0 +1,18 @@
+package com.Practice.Selenium112024;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestExample {
+	private static final Logger logger = LogManager.getLogger(TestExample.class);
+
+	public static void main(String[] args) {
+		logger.trace("trace");
+		logger.debug("debug");
+		logger.info("info");
+		logger.warn("warn");
+		logger.error("error");
+		logger.fatal("fatal");
+		System.out.println("Exit the program");
+	}
+}
