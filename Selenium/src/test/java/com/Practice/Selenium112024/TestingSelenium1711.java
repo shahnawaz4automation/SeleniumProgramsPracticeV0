0e0e1322a11415b9bcef5306b918d7d90a1af913
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-11-19 22:32:52 +0530

    r2qrqw commit

--- Selenium/src/test/java/com/Practice/Selenium112024/TestingSelenium1711.java
+++ Selenium/src/test/java/com/Practice/Selenium112024/TestingSelenium1711.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestingSelenium1711 {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-
 		driver.get("https://example.com");
 
 		// Locate the element
