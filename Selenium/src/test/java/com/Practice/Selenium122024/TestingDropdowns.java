12ede01fc4a6b4d0cff3efb4abb69f6f9a1e8939
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-12-23 14:16:42 +0530

    Create TestingDropdowns.java

---
+++ Selenium/src/test/java/com/Practice/Selenium122024/TestingDropdowns.java
@@ -0,0 +1,39 @@
+package com.Practice.Selenium122024;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestingDropdowns {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Hi");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+		WebElement dropdown = driver.findElement(By.xpath("//select[@id='country']"));
+		Select select = new Select(dropdown);
+		
+		select.selectByIndex(2);
+		Thread.sleep(1000);
+		select.selectByIndex(0);
+		Thread.sleep(1000);
+		select.selectByVisibleText("India");
+		Thread.sleep(1000);
+		select.selectByValue("germany");
+		
+		List<WebElement> dropdownValues = driver.findElements(By.xpath("//select[@id='country']"));
+		System.out.println(dropdownValues.size());
+		
+		
+	}
+
+}
