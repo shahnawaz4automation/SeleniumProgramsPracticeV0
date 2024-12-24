cfb2b3d7dcf10d0ce2de5cf8e422e7291f260952
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-12-24 17:50:29 +0530

    r9842uf commit

--- Selenium/src/test/java/com/Practice/Selenium122024/BootstrapDropdown.java
+++ Selenium/src/test/java/com/Practice/Selenium122024/BootstrapDropdown.java
@@ -2,15 +2,16 @@ package com.Practice.Selenium122024;
 
 import java.time.Duration;
 import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BootstrapDropdown {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver = new EdgeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
@@ -24,13 +25,13 @@ public class BootstrapDropdown {
 
 		for (WebElement checkBox : checkBoxOptions) {
 			String option = checkBox.getText().trim();
-			//System.out.println(option);
+			// System.out.println(option);
 			if (option.equals("Java")) {
 				Thread.sleep(1000);
 				if (!checkBox.isSelected()) {
 					checkBox.click();
 				}
-				
+
 				System.out.println("Checkbox is selected");
 			}
 		}
