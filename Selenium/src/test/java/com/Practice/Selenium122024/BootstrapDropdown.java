ec4bf5e4fef3f6e6f22cd3fb98b5b57f0f7e6f35
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-12-24 12:55:51 +0530

    3o2if	j commit

--- Selenium/src/test/java/com/Practice/Selenium122024/BootstrapDropdown.java
+++ Selenium/src/test/java/com/Practice/Selenium122024/BootstrapDropdown.java
@@ -9,26 +9,36 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class BootstrapDropdown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
-		
+
 		driver.findElement(By.xpath("//button[@class='multiselect dropdown-toggle btn btn-default']")).click();
-		List<WebElement> checkBoxOptions = driver.findElements(By.xpath("//ul[@class='multiselect-container dropdown-menu']//label[@class='checkbox']"));
+		List<WebElement> checkBoxOptions = driver
+				.findElements(By.xpath("//ul[@class='multiselect-container dropdown-menu']//label[@class='checkbox']"));
 		int size = checkBoxOptions.size();
-		
+
 		System.out.println(size);
-		
-		for(WebElement checkBox: checkBoxOptions) {
-			System.out.println(checkBox.getText());
+
+		for (WebElement checkBox : checkBoxOptions) {
+			String option = checkBox.getText().trim();
+			if (option.equals("Java")) {
+				Thread.sleep(1000);
+				if (!checkBox.isSelected()) {
+					checkBox.click();
+				}
+
+				System.out.println("Checkbox is selected");
+			}
 		}
-		
+
 		System.out.println(size);
-		
-		driver.quit();
-		//driver.findElement(By.xpath("//button[@class='multiselect dropdown-toggle btn btn-default']")).click();
+
+		// driver.quit();
+		// driver.findElement(By.xpath("//button[@class='multiselect dropdown-toggle btn
+		// btn-default']")).click();
 	}
 
 }
