16bc4f35b30761595dfce3017d38fc8764436e43
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-03-10 23:39:03 +0530

    432423 commit

---
+++ Selenium/src/test/java/com/Practice/Selenium032025/Test1.java
@@ -0,0 +1,17 @@
+package com.Practice.Selenium032025;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+		String URL = "https://www.google.com";
+		String URL2 = "https://www.facebook.com";
+		URL=URL2;
+		
+		System.out.println(URL);
+		driver.get(URL);
+	}
+}
