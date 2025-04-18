ad2a2e5b6e2a05d781acf360dcd835dd735ae6c0
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-04-18 21:24:16 +0530

    98ruwjf commit

--- Selenium/src/test/java/com/Practice/Selenium032025/Test1.java
+++ Selenium/src/test/java/com/Practice/Selenium032025/Test1.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test1 {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		
+
 		String URL = "https://www.google.com";
 		String URL2 = "https://www.facebook.com";
-		URL=URL2;
-		
+		URL = URL2;
+
 		System.out.println(URL);
 		driver.get(URL);
 	}
