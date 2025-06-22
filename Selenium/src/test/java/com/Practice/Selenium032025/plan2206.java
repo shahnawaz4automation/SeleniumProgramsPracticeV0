49acad0bcc9c212bdbf13f57a6ad083709462861
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-22 21:56:10 +0530

    98jfsda commit

---
+++ Selenium/src/test/java/com/Practice/Selenium032025/plan2206.java
@@ -0,0 +1,13 @@
+package com.Practice.Selenium032025;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class plan2206 {
+	@Test
+	public void launchBrowser() {
+		Scanner scan = new Scanner(System.in);
+		String browser = scan.next();
+	}
+}
