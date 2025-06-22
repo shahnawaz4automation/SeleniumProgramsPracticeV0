894f213e0730dff8ecdfa8b9d4393c48c592176a
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-22 22:00:51 +0530

    98fuajs commit

--- Selenium/src/test/java/com/Practice/Selenium032025/plan2206.java
+++ Selenium/src/test/java/com/Practice/Selenium032025/plan2206.java
@@ -2,12 +2,20 @@ package com.Practice.Selenium032025;
 
 import java.util.Scanner;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class plan2206 {
+	
 	@Test
 	public void launchBrowser() {
 		Scanner scan = new Scanner(System.in);
 		String browser = scan.next();
+		
+		switch (browser) {
+		case "chrome" : WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com"); break;
+		}
 	}
 }
