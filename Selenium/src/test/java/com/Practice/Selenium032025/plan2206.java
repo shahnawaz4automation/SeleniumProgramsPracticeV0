e041055ee91fe0b18bbb6c0bf584365198f18980
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-22 22:05:43 +0530

    8fjdsiafi commit

--- Selenium/src/test/java/com/Practice/Selenium032025/plan2206.java
+++ Selenium/src/test/java/com/Practice/Selenium032025/plan2206.java
@@ -4,18 +4,33 @@ import java.util.Scanner;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class plan2206 {
-	
+	public WebDriver driver;
+
 	@Test
 	public void launchBrowser() {
 		Scanner scan = new Scanner(System.in);
 		String browser = scan.next();
-		
+
 		switch (browser) {
-		case "chrome" : WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com"); break;
+		case "chrome":
+			driver = new ChromeDriver();
+			driver.get("https://www.google.com");
+			break;
+
+		case "firefox":
+			driver = new FirefoxDriver();
+			driver.get("https://www.google.com");
+			break;
+			
+		case "edge":
+			driver = new EdgeDriver();
+			driver.get("https://www.google.com");
+			break;
 		}
 	}
 }
