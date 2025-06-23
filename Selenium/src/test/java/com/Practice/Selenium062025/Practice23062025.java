e0506f55914e2cc38753cadda9b9f3a5e9b32a45
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-23 21:42:57 +0530

    rt3wges commit

---
+++ Selenium/src/test/java/com/Practice/Selenium062025/Practice23062025.java
@@ -0,0 +1,13 @@
+package com.Practice.Selenium062025;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Practice23062025 {
+	@Test
+	public void program() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+}
