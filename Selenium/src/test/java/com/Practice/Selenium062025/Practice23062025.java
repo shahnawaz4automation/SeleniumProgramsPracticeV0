9b349099e99634f032993439e9ee696f68eadcb8
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-23 22:07:41 +0530

    r576uhi commit

--- Selenium/src/test/java/com/Practice/Selenium062025/Practice23062025.java
+++ Selenium/src/test/java/com/Practice/Selenium062025/Practice23062025.java
@@ -1,6 +1,8 @@
 package com.Practice.Selenium062025;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -8,6 +10,12 @@ public class Practice23062025 {
 	@Test
 	public void program() {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.com");
+		driver.get("https://www.amazon.in");
+		driver.manage().window().maximize();
+		WebElement books = driver.findElement(By.xpath("//a[@href=\"/Books/b/?ie=UTF8&node=976389031&ref_=nav_cs_books\"]"));
+		String innerText = books.getText();
+		System.out.println("Text is: " + innerText);
+		
+		driver.close();
 	}
 }
