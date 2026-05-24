723e8f3ec79e2fe368db2a68a6e342e5c9081ea8
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-05-24 22:07:28 +0530

    Update

--- Selenium/src/test/java/com/Practice/Selenium2026/Program1705.java
+++ Selenium/src/test/java/com/Practice/Selenium2026/Program1705.java
@@ -9,14 +9,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Program1705 {
-	@Test
-	public void launchBrowser(){
+	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		
 		String url = "https://omayo.blogspot.com/";
+		//driver = null;
 		driver.get(url);
 		driver.manage().window().maximize();
-		WebElement textArea2 = driver.findElement(By.xpath("//textarea[contains(text(),'The cat was playing in the garden.')]"));
+		WebElement textArea2 = driver.findElement(By.xpath("//textarea[contains(text(),'The cat wa009292s playing in the garden.')]"));
 		textArea2.clear();
 		textArea2.sendKeys("searchArea");		
 		driver.findElement(By.xpath("//input[@value='male']")).click();		
@@ -26,5 +26,4 @@ public class Program1705 {
 		driver.findElement(By.xpath("//input[@value='female']")).click();
 		driver.quit();
 	}
-	
-}
+	}
